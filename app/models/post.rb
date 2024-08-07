4fda02e1a5cb57838d0c00e486a43304b80097d4
tonniegeetz <tonniegeetz@gmail.com>
2024-08-07 14:11:45 +0300

    validattions

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %i[fiction non_fiction] }
+
+  validate :clickbait
+
+  private
+
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+  def clickbait
+    return unless CLICKBAIT_PATTERNS
+
+    errors.add(:title, 'must be clickbait-y')
+  end
 end
